b22fbf56b61f4f20c3535ab529abc2f38590bba9
krastogi <krastogi@expedia.com>
2018-07-03 16:29:45 +0530

    Passing release version as property to pom

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -23,14 +23,15 @@ then
     echo $GPG_OWNERTRUST | base64 --decode | $GPG_EXECUTABLE --import-ownertrust
 fi
 
-if [ ! -z "$TRAVIS_TAG" ]
+if [ ! -z "${TRAVIS_TAG}" ]
 then
-    SKIP_GPG_SIGN=false
-    echo "travis tag is set -> updating pom.xml <version> attribute to $TRAVIS_TAG"
-    ./mvnw --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=$TRAVIS_TAG 1>/dev/null 2>/dev/null
+    echo "travis tag is set -> updating pom.xml <version> attribute to ${TRAVIS_TAG}"
+    mvn clean -Drelease.version=${TRAVIS_TAG} deploy --settings .travis/settings.xml -Dgpg.skip=false -DskipTests=true -B -U
+    SUCCESS=$?
 else
-    SKIP_GPG_SIGN=true
     echo "no travis tag is set, hence keeping the snapshot version in pom.xml"
+    mvn clean deploy --settings .travis/settings.xml -Dgpg.skip=true -DskipTests=true -B -U
+    SUCCESS=$?
 fi
 
 ./mvnw clean deploy --settings .travis/settings.xml -Dgpg.skip=$SKIP_GPG_SIGN -DskipTests=true -B -U
