fa8890f58ede4d5121f79e5b8e62c1e6db0ac14a
krastogi <krastogi@expedia.com>
2019-04-01 10:47:39 +0530

    Improving precision for HdrHistogram

--- timeseries-aggregator/src/main/scala/com/expedia/www/haystack/trends/aggregation/TrendHdrHistogram.scala
+++ timeseries-aggregator/src/main/scala/com/expedia/www/haystack/trends/aggregation/TrendHdrHistogram.scala
@@ -1,3 +1,21 @@
+/*
+ *
+ *     Copyright 2019 Expedia, Inc.
+ *
+ *      Licensed under the Apache License, Version 2.0 (the "License");
+ *      you may not use this file except in compliance with the License.
+ *      You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *      Unless required by applicable law or agreed to in writing, software
+ *      distributed under the License is distributed on an "AS IS" BASIS,
+ *      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *      See the License for the specific language governing permissions and
+ *      limitations under the License.
+ *
+ */
+
 package com.expedia.www.haystack.trends.aggregation
 
 import java.nio.ByteBuffer
@@ -7,6 +25,11 @@ import com.expedia.www.haystack.trends.config.AppConfiguration
 import com.expedia.www.haystack.trends.config.entities.HistogramMetricConfiguration
 import org.HdrHistogram.Histogram
 
+/**
+  * Wrapper over hdr Histogram. Takes care of unit mismatch of histogram and the other systems.
+  *
+  * @param hdrHistogram : instance of hdr Histogram
+  */
 class TrendHdrHistogram(hdrHistogram: Histogram) {
 
   def this(histogramConfig: HistogramMetricConfiguration) = this(new Histogram(histogramConfig.maxValue, histogramConfig.precision))
@@ -55,6 +78,7 @@ class TrendHdrHistogram(hdrHistogram: Histogram) {
 
 object TrendHdrHistogram {
   private val histogramUnit = AppConfiguration.histogramMetricConfiguration.unit
+
   def normaliseHistogramValue(value: Long): Long = {
     if (histogramUnit.isMillis) {
       TimeUnit.MICROSECONDS.toMillis(value)
